c36404ec86ad5477fa80145b1dc26866c72a5089
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-10 04:55:30 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,27 @@
 def new_hash
   # return an empty hash
+  empty_hash = {}
+  
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  tv_shows = {
+    horror: "Haunting of Hill House"
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneers = {
+    name: "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  generator = {
+    id: number
+  }
 end
